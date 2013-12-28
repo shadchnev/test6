a7f26ec8fb6bb2eb97fab26220a385ef12527ccd
Simon Woolf <simon@simonwoolf.net>
2013-12-28 22:15:14 +0000

    added integration test from spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,4 +3,14 @@ describe 'terminal paint' do
     STDOUT.should_receive(:puts).with(">")
     load 'paint.rb'
   end
+
+  xit 'should confirm to first example in the spec' do 
+    STDIN.should_receive(:read).and_return("I 5 6", "L 2 3 A", "")
+    STDOUT.should_receive(:puts).with("OOOOO" +
+                                      "OOOOO" +
+                                      "OAOOO" +
+                                      "OOOOO" +
+                                      "OOOOO" +
+                                      "OOOOO")
+  end
 end

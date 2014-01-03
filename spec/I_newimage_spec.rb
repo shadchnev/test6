a4d1c9d09a285eab48ce96ca9bbb1a683f238b55
Simon Woolf <simon@simonwoolf.net>
2014-01-03 14:24:49 +0000

    incomplete new image spec

--- spec/I_newimage_spec.rb
+++ spec/I_newimage_spec.rb
@@ -6,4 +6,10 @@ describe Image do
     expect(image.to_s.split(/\n/).first.length).to eq 20
     expect(image.to_s.split(/\n/).length).to eq 10
   end
+
+  context 'size limits' do
+    it 'should have a maximum size of 250 in each dimension' do
+      expect(Image.new(columns: 250, rows: 250)).to_not be_nil
+    end
+  end
 end
